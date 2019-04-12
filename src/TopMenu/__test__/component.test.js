ee2d570c97e87c5b549f521a61d0b15117126d69
yurii.lychak@gmail.com <Eskeajbymvebfe4>
2019-04-12 17:54:05 +0300

    Update test for Top menu

--- src/TopMenu/__test__/component.test.js
+++ src/TopMenu/__test__/component.test.js
@@ -4,11 +4,22 @@ import Button from "../component/Button";
 import MenuBackground from "../component/MenuBackground";
 import MenuItem from "../component/MenuItem";
 import MenuList from "../component/MenuList";
+import MenuSection from "../component/MenuSection";
 import ToolBar from "../component/ToolBar";
+import TopMenu from "../component";
 import React from "react";
+import { initialState } from '../reducer';
 import { createMount } from '@material-ui/core/test-utils';
 
 describe('TopMenu test',()=> {
+    it ( 'TopMenu snapshot', () => {
+        const wrapper = createMount()(
+            <TopMenu {...initialState} />
+        );
+
+        expect(wrapper.html()).toMatchSnapshot();
+    });
+
     it ( 'Button snapshot', () => {
         const wrapper = createMount()(
             <Button>test</Button>
@@ -16,6 +27,7 @@ describe('TopMenu test',()=> {
 
         expect(wrapper.html()).toMatchSnapshot();
     });
+
     it ( 'AppBar snapshot', () => {
         const wrapper = createMount()(
             <AppBar>
@@ -101,6 +113,89 @@ describe('TopMenu test',()=> {
             expect(wrapper.props().onClick).toHaveBeenCalled();
             expect(wrapper.props().onClick).toHaveBeenCalledWith(0);
         });
+    });
+
+    describe('MenuSection test', () => {
+        const props = {
+            "id": 1,
+            "locale": "TopMenu_File",
+            "useToggles": false,
+            "sections": [
+                {
+                    "id": 0,
+                    "locale": "TopMenu_NewProject",
+                    "hotKey": "Ctr+N",
+                    "isToggle": false
+                },
+                {
+                    "id": 1,
+                    "locale": "TopMenu_OpenProject",
+                    "hotKey": "Ctr+O",
+                    "isToggle": false
+                },
+                {
+                    "id": 2,
+                    "locale": "TopMenu_SaveAll",
+                    "hotKey": "Ctr+S",
+                    "isToggle": false
+                },
+                {
+                    "id": 3,
+                    "locale": "TopMenu_Export",
+                    "hotKey": "Ctr+E",
+                    "isToggle": true
+                },
+                {
+                    "id": 17,
+                    "locale": "TopMenu_Publish",
+                    "hotKey": "Ctr+P",
+                    "isToggle": false
+                },
+                {
+                    "id": 4,
+                    "locale": "TopMenu_Quit",
+                    "hotKey": "Ctr+Q",
+                    "isToggle": false
+                }
+            ],
+            onOpen: jest.fn(),
+            onClose: jest.fn(),
+            toggledSections: [3],
+            isOpen: true
+        };
+
+        const wrapper = createMount()(
+            <MenuSection
+                {...props}
+            />
+        );
+
+        it ('snapshot open', () => {
+            expect(wrapper.html()).toMatchSnapshot();
+        });
+
+        it ('snapshot closed', () => {
+            const closedProps = {
+                ...props,
+                isOpen: false
+            };
+            const closedWrapper = createMount()(
+                <MenuSection
+                    {...closedProps}
+                />
+            );
+            expect(closedWrapper.html()).toMatchSnapshot();
+        });
+
+        it ('click', () => {
+            jest.spyOn(wrapper.props(), "onOpen");
+
+            wrapper.find("button").first().simulate("click");
+
+            expect(wrapper.props().onOpen).toHaveBeenCalled();
+            expect(wrapper.props().onOpen).toHaveBeenCalledWith(1);
+        });
+
     });
 
 });
